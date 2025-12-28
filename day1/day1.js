8fc5ae95ed5885ea99d8fc339e05cd37d3c6b842
Yusuf Mıraz Esen <yusufmiraz04@gmail.com>
2025-12-28 17:08:34 +0300

    Initial commit

---
+++ day1/day1.js
@@ -0,0 +1,13 @@
+//using console log()
+
+console.log('Hello world!')
+console.log("Hello world!")
+console.log(`Hello world!`)
+
+//mathematical calculations
+console.log(2 + 3) // Addition
+console.log(12 - 5) // Subtraction
+console.log(6 * 3) // Multiplication
+console.log(8 / 4) // Division
+console.log(7 % 2) // Modulus - finding remainder
+console.log(5 ** 2) // Exponentiation
---
+++ day1/introduction.js
@@ -0,0 +1,1 @@
+console.log('Bu yazı introduction.js dosyasında')
