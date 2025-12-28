6eba8e83813243142d2030880a61ad3eb1466a6b
Yusuf Mıraz Esen <yusufmiraz04@gmail.com>
2025-12-28 18:00:48 +0300

    Initial commit

--- day1/day1.js
+++ day1/day1.js
@@ -11,3 +11,67 @@ console.log(6 * 3) // Multiplication
 console.log(8 / 4) // Division
 console.log(7 % 2) // Modulus - finding remainder
 console.log(5 ** 2) // Exponentiation
+
+
+let firstName
+console.log(firstName) // undefined, because it is not assigned to a value yet
+
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number - float tipler de number olarak adlandırılır
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+
+
+//variables
+let studentName = 'Yusuf'
+let city = 'Izmir'
+let studentAge = 23
+let isMarried = false
+console.log(studentName, city, studentAge, isMarried)
+
+const PI = 3.14
+const boilingPoint = 100
+console.log(PI, boilingPoint)
+
+//EXERCISES
+//Comments can make code readable
+//Welcome to 30DyasOfJavascript
+
+/* Comments can make code readable, easy to reuse and informative */
+
+let exercise = 'Example'
+let exercise2 = false
+let exercise3 = undefined
+let exercise4 = null
+
+console.log(typeof 'yusuf')
+console.log(typeof false)
+console.log(typeof 12)
+
+let ogrenciad
+let ogrenciSoyad
+let ogrenciNo
+let ogrenciSehir
+
+let ogrenciNot = 79
+let ogrenciDers = 'Programlama'
+let hocaAd = 'Can'
+let dersGecme = true
+console.log(ogrenciNot, ogrenciDers, hocaAd, dersGecme)
+
+let ad = 'Yusuf'
+let soyad = 'Esen'
+let married = false
+const country = 'Turkey'
+let age = 23
+console.log(ad, soyad, married, country, age)
+
+let fName = 'Yusuf', surname = 'Esen'
+console.log(fName, surname)
+
+let myAge = 23
+let yourAge = 21
+console.log('I am', myAge, 'years old.')
+console.log('You are', yourAge, 'years old.')
