2033f7966cc7ac68e2d2e28723da2cb7bfb78dfc
Yusuf Mıraz Esen <yusufmiraz04@gmail.com>
2025-12-30 17:04:13 +0300

    Initial commit

---
+++ day2/day2.js
@@ -0,0 +1,31 @@
+//primitive - değerlerine göre karşılaştırır
+
+let numOne = 3
+let numTwo = 3
+console.log(numOne == numTwo)
+
+let js = 'Javascript'
+let py = 'Python'
+console.log(js==py)
+
+//değiştirilebilir - arrays
+let nums = [1, 2, 3]
+nums[0]=10
+console.log(nums)
+
+//math object
+const num = Math.floor(Math.random() * 11)
+console.log(num)
+
+let numbOne = '3'
+let numbTwo = 5
+let addNumb = numbOne + numbTwo
+console.log(addNumb)
+/* artı ile birleştirmek artık eski ve çok da kullanılmayan bir yöntem */
+
+
+//string methods
+//length
+let ilkAd = 'yusuf'
+console.log(ilkAd.length)
+
